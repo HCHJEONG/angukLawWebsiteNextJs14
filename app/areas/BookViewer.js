2253cc39f9bfb55c3a03acdd7324fbb72163b15b
HCHJEONG <hcjeong@kakao.com>
2024-02-27 17:39:11 +0900

    try again

---
+++ app/areas/BookViewer.js
@@ -0,0 +1,77 @@
+import { useLayoutEffect } from "react";
+// import { Previewer } from "pagedjs";
+import { Previewer, Chunker } from "../../utils/paged-penvot";
+
+const BookViewer = () => {
+  
+  useLayoutEffect(() => {
+    
+    const previewer = new Previewer();
+    previewer
+      .preview(
+        document.querySelector("#pagedjsdocroot").innerHTML,
+        [
+          {_: 
+            `
+              @page {
+                size: A4;
+                margin: 30mm 30mm 30mm 30mm;
+                @bottom-center { content: counter(page) " / " counter(pages); }
+                
+                @footnote {
+                  float: bottom;
+                }
+                
+                marks: crop cross;
+              }
+                    
+              .EditorTheme__footnotescript {
+                float: footnote;
+                position: footnotes;
+              }
+    
+              ::footnote-call {
+                content: counter(footnote, loweralpha);
+                color: green;
+                font-weight: 700;
+                font-size: 12px;
+                line-height: 0;
+    
+                vertical-align: super;
+              }
+    
+              ::footnote-marker {
+                content: "";
+                color: black;
+                font-weight: 500;
+                font-size: 15px;
+                line-height: 0;
+              } 
+                      
+            `  
+          }
+        ],      
+        document.querySelector("#ebookreader")
+      )
+      .then((flow) => {
+        console.log("ebookreader rendered, total pages", flow.total, { flow });
+        if (document.querySelectorAll(".pagedjs_pages").length > 1){
+          document.querySelector(".pagedjs_pages").innerHTML='';
+          document.querySelector(".pagedjs_pages").remove();
+        }
+      });
+
+    return () => {
+      document.head
+        .querySelectorAll("[data-pagedjs-inserted-styles]")
+        .forEach((e) => e.parentNode?.removeChild(e));
+    };
+
+  }, []);
+
+  // return <div></div>;
+  return null;
+  
+};
+
+export default BookViewer;
--- app/areas/page.tsx
+++ app/areas/page.tsx
@@ -1,112 +1,29 @@
+/* eslint-disable @next/next/no-img-element */
 'use client';
 
 import styles from "./page.module.css";
-import {Previewer} from "../../utils/paged-penvot";
-import { useEffect, useRef } from "react";
+// import { Previewer, Chunker } from "../../utils/paged-penvot";
+// import { useEffect, useRef } from "react";
+
+import React from "react";
+import BookViewer from "./BookViewer";
+import Book from "./Book";
 
 export default function Areas () {
 
-  const ref = useRef<HTMLDivElement>(null);
-  useEffect(()=>{
-    new Previewer().preview(
-      
-      (ref.current?.innerHTML === null || ref.current!.innerHTML === '') 
-      ? 
-      '<div></div>' 
-      : 
-      ref.current!.innerHTML,
-      [
-        {_: 
-          `
-            @page {
-              size: A4;
-              margin: 30mm 30mm 30mm 30mm;
-              @bottom-center { content: counter(page) " / " counter(pages); }
-              
-              @footnote {
-                float: bottom;
-              }
-              
-              marks: crop cross;
-            }
-                  
-            .EditorTheme__footnotescript {
-              float: footnote;
-              position: footnotes;
-            }
-  
-            ::footnote-call {
-              content: counter(footnote, loweralpha);
-              color: green;
-              font-weight: 700;
-              font-size: 12px;
-              line-height: 0;
-  
-              vertical-align: super;
-            }
-  
-            ::footnote-marker {
-              content: "";
-              color: black;
-              font-weight: 500;
-              font-size: 15px;
-              line-height: 0;
-            } 
-                    
-          `  
-        }
-      ],      
-    )
-    .then((flow: any) => {
-      console.log("preview rendered, total pages", flow.total, { flow });
-    });
-    
-  }, [ref])
-
-  return (
-    <div
-      className={styles.areas_wrapper}
-      ref={ref} 
-    >
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptatibus molestias, eaque nisi aliquam aspernatur sapiente quod deleniti nulla beatae voluptates ipsa ut libero perspiciatis cumque quidem porro amet maxime.
-      </p>
+  return (    
+    <div className={styles.areas_wrapper}>
+      <div 
+        id="pagedjsdocroot" 
+        style={{ 
+          display: "none",
+        }}
+      >
+        <Book></Book>
+      </div>
+      <div id="ebook"></div>
+      <BookViewer></BookViewer>
     </div>
   )
+
 }
